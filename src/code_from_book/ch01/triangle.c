2d1f0d4029048acc1bc2b6293a5aac4d7a1f4e90
Chauveau Paul <paul.chauveau@etu.u-bordeaux.fr>
2024-02-02 18:23:13 +0100

    reading intro

--- src/code_from_book/ch01/triangle.c
+++ src/code_from_book/ch01/triangle.c
@@ -1,6 +1,14 @@
-int triangle (int width, int height){
-int array[5] = {0,1,2,3,4};
-int area;
-area = width * height/2;
-return (area);
+
+int triangle (int width, int height)
+{
+    int array[5] = {0,1,2,3,4};
+    int area;
+    area = width * height/2;
+    return (area);
+}
+
+int main()
+{
+    int value = triangle (2, 3);
+    return 0;
 }
---
+++ src/code_from_book/ch01/triangle.s
@@ -0,0 +1,57 @@
+	.file	"triangle.c"
+	.text
+	.globl	triangle
+	.type	triangle, @function
+triangle:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -36(%rbp)
+	movl	%esi, -40(%rbp)
+	movl	$0, -32(%rbp)
+	movl	$1, -28(%rbp)
+	movl	$2, -24(%rbp)
+	movl	$3, -20(%rbp)
+	movl	$4, -16(%rbp)
+	movl	-36(%rbp), %eax
+	imull	-40(%rbp), %eax
+	movl	%eax, %edx
+	shrl	$31, %edx
+	addl	%edx, %eax
+	sarl	%eax
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	triangle, .-triangle
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$3, %esi
+	movl	$2, %edi
+	call	triangle
+	movl	%eax, -4(%rbp)
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Debian 12.2.0-14) 12.2.0"
+	.section	.note.GNU-stack,"",@progbits
